12f514a3b0a12340737d190dab85cdfb0fe8c2d2
lixianfu <lixianfu1@outlook.com>
2020-06-30 23:51:22 +0800

    learning netty and add maven support

---
+++ src/main/java/unit15_javaio/c5/RedirectIn.java
@@ -0,0 +1,26 @@
+package unit15_javaio.c5;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class RedirectIn {
+	public static void main(String[] args) {
+		try(
+			FileInputStream fis = new FileInputStream("./src/unit15_javaio/c5/RedirectIn.java"))
+		{
+			System.setIn(fis);
+			@SuppressWarnings("resource")
+			Scanner sc = new Scanner(System.in);
+			sc.useDelimiter("\n");
+			while(sc.hasNext())
+			{
+				System.out.println("键盘输入的内容是：" + sc.next());
+			}
+		}
+		catch (IOException ex) 
+		{
+			ex.printStackTrace();
+		}
+	}
+}
