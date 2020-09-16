2c8511b9e3669c4b76d7ce9c64bcefb2385e25e8
Alvin.li <1109075867@qq.com>
2020-09-16 16:40:26 +0800

    io

--- src/main/java/unit15_javaio/c3/FileInputStreamTest.java
+++ src/main/java/unit15_javaio/c3/FileInputStreamTest.java
@@ -3,9 +3,13 @@ package unit15_javaio.c3;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+/**
+ * @author Alvin.Li
+ */
 public class FileInputStreamTest {
 	public static void main(String[] args) throws IOException {
-		FileInputStream fis = new FileInputStream("./src/unit15_javaio/c3/FileInputStreamTest.java");
+		String relativelyPath = System.getProperty("user.dir");
+		FileInputStream fis = new FileInputStream(relativelyPath + "/src/main/java/unit15_javaio/c3/FileInputStreamTest.java");
 		byte[] bbuf = new byte[1024];
 		int hasRead = 0;
 		while ((hasRead = fis.read(bbuf)) > 0) {
